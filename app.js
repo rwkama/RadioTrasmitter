8f1c66a667c0919cb1de3bbbafb956cd7098776a
Carlos Andres Rodriguez <rwkama27@gmail.com>
2021-01-03 19:39:37 -0300

    Preparing to publish

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ const factory = require("./RadioTransmitter/logic/FactoryLogic").FactoryLogic;
 const Advertiser = require("./RadioTransmitter/shared/entityshared/Advertiser").Advertiser;
 //EXAMPLE ADVERTISERS----------------------------------------
 
-//factory.getLogicAdvertiser().getAdvertisers().then(data => {
-//        console.log(data)
-//    });
-//factory.getLogicAdvertiser().getAdvertiserByNameLetter("s").then(data => {
-//        console.log(data)
-//    });
-//factory.getLogicAdvertiser().getAdvertiser(89878).then(data => {
-//    console.log(data)
+factory.getLogicAdvertiser().getAdvertisers().then(data => {
+        console.log(data)
+    });
+factory.getLogicAdvertiser().getAdvertiserByNameLetter("s").then(data => {
+        console.log(data)
+    });
+factory.getLogicAdvertiser().getAdvertiser(89878).then(data => {
+    console.log(data)
 
-//    });
-//let datatypeAdvertiser = new Advertiser(898788, "NewAdvertiser", "Vasd", "545556546");
+    });
+let datatypeAdvertiser = new Advertiser(898788, "NewAdvertiser", "Vasd", "545556546");
 
-//factory.getLogicAdvertiser().addAdvertiser(datatypeAdvertiser).then(data => {
-//        console.log(data)
-//    });
-//factory.getLogicAdvertiser().deleteAdvertiser(datatypeAdvertiser).then(data => {
-//        console.log(data)
-//    });
-//factory.getLogicAdvertiser().updateAdvertiser(datatypeAdvertiser).then(data => {
-//        console.log(data)
-//    });
+factory.getLogicAdvertiser().addAdvertiser(datatypeAdvertiser).then(data => {
+        console.log(data)
+    });
+factory.getLogicAdvertiser().deleteAdvertiser(datatypeAdvertiser).then(data => {
+        console.log(data)
+    });
+factory.getLogicAdvertiser().updateAdvertiser(datatypeAdvertiser).then(data => {
+        console.log(data)
+    });
 
